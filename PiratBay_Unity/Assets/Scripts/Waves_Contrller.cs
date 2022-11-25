22963178bce4ae5972ce6f1a182fcb6376c97084
Jose Daniel Rave Robayo <jrave@ucm.es>
2022-11-25 20:48:16 +0100

    Sonido de agua loopeado (he tenido que mirar como hacer un loop) y arregladas posiciones bugueadas con la ayuda de tester Miriam

--- PiratBay_Unity/Assets/Scripts/Waves_Contrller.cs
+++ PiratBay_Unity/Assets/Scripts/Waves_Contrller.cs
@@ -71,7 +71,7 @@ public class Waves_Contrller : MonoBehaviour
     private GameObject nearestObject(GameObject[] objecs){
         GameObject nearestObject = null;
 
-        Vector3 P = oceanEmitter.transform.position;
+        Vector3 P = transform.position;
 
         float minDis = Mathf.Infinity;
         for (int i = 0; i < objecs.Length; i++)
