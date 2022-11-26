c99fe8559e510b6ce00d71f87a8a28b2e2c0bcae
Ángel López <angelo06@ucm.es>
2022-11-26 18:06:34 +0100

    Un pasito palante maria

---
+++ PiratBay_Unity/Assets/Scripts/stepController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stepController : MonoBehaviour
+{
+    FMODUnity.StudioEventEmitter stepEmitter;
+
+
+    private void Start()
+    {
+        stepEmitter = GetComponent <FMODUnity.StudioEventEmitter>();
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
+        {
+            stepEmitter.EventInstance.setParameterByName("Moving", 1);
+
+        }
+
+        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
+        {
+            stepEmitter.EventInstance.setParameterByName("Moving", 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            stepEmitter.EventInstance.setParameterByName("Run", 1);
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            stepEmitter.EventInstance.setParameterByName("Run", 0);
+        }
+    }
+}
