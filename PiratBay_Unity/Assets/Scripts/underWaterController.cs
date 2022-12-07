eee3682c57ad77645485c7b1dbbb25b6d7c35f75
Ángel López <62568478+angelo066@users.noreply.github.com>
2022-12-07 22:45:48 +0100

    efecto bajoagua salvo pasos

---
+++ PiratBay_Unity/Assets/Scripts/underWaterController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class underWaterController : MonoBehaviour
+{
+    public GameObject head;
+
+    private void Update()
+    {
+
+
+        if (transform.position.y > head.transform.position.y)
+        {
+            float dist = transform.position.y - head.transform.position.y;
+            FMODUnity.RuntimeManager.StudioSystem.setParameterByName("underWater", 0.6f);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.GetComponent<stepController>() != null) FMODUnity.RuntimeManager.StudioSystem.setParameterByName("underWater", 0.0f);
+    }
+}
