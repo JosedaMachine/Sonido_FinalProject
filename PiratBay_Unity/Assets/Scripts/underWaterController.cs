51fcc7de7bbdaa2984c7f0793f439887fef33bd6
JosedaMachine <jrave@ucm.es>
2022-12-08 14:23:21 +0100

    Arreglillos de bajo el agua

--- PiratBay_Unity/Assets/Scripts/underWaterController.cs
+++ PiratBay_Unity/Assets/Scripts/underWaterController.cs
@@ -1,23 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Rendering.PostProcessing;
 public class underWaterController : MonoBehaviour
 {
     public GameObject head;
+    public float thresholdDistance;
+    [Range(0, -200)]
+    public int rangeBlueFilter;
+
+    public PostProcessVolume m_Volume;
+    ColorGrading colorGrading_;
+
+    private void Start()
+    {
+        m_Volume.profile.TryGetSettings<ColorGrading>(out  colorGrading_) ;
+        Debug.Log(colorGrading_.mixerRedOutBlueIn.value);
+    }
 
     private void Update()
     {
-
         if (transform.position.y > head.transform.position.y)
         {
-            float dist = transform.position.y - head.transform.position.y;
+            float dist = Mathf.Abs(transform.position.y - head.transform.position.y);
+            dist = Mathf.Clamp(dist / thresholdDistance, 0.0f, 1.0f);
+            colorGrading_.mixerRedOutBlueIn.value = dist * rangeBlueFilter;
+            //Debug.Log(dist);
             FMODUnity.RuntimeManager.StudioSystem.setParameterByName("underWater", dist);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<stepController>() != null)
+        {
+            this.enabled = true;
+            Debug.Log("In water");
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if(other.GetComponent<stepController>() != null) FMODUnity.RuntimeManager.StudioSystem.setParameterByName("underWater", 0.0f);
+        if (other.GetComponent<stepController>() != null){
+
+            this.enabled = false;
+            Debug.Log("outside");
+            FMODUnity.RuntimeManager.StudioSystem.setParameterByName("underWater", 0.0f);
+        }
     }
 }
